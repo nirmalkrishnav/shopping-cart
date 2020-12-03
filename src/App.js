e639e4fde8cabadb76c04ab3289656ee1bcf8937
meuequalsD <nirmalkrishnav@gmail.com>
2020-12-03 22:46:07 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div className="flex flex-col min-h-screen ">
+
+      <header className="bg-green-200">
+        <a href="/">Shopping Cart</a>
+      </header>
+      <main className="flex-grow">
+        Content
+      </main>
+      <footer>
+        footer
+      </footer>
+    </div>
+  );
+}
+
+export default App;
