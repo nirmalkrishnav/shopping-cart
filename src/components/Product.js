cca0303ec85c2accb3a8ce910166b0e02986c417
meuequalsD <nirmalkrishnav@gmail.com>
2020-12-04 10:06:09 +0530

    introducing cards

--- src/components/Product.js
+++ src/components/Product.js
@@ -4,15 +4,19 @@ export default class Product extends Component {
     render() {
         return (
             <div>
-                <ul>
-                    {this.props.products.map(prod => {
-                        return <li key={prod.id}>
-                            {prod.title}
-                            <br />
-                            ${prod.price}
-                        </li>
-                    })}
-                </ul>
+                {this.props.products.map(prod => {
+                    return (
+                        <div key={prod.id} className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+                            <div className="flex-shrink-0">
+                                <img className="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo" />
+                            </div>
+                            <div>
+                                <div className="text-xl font-medium text-black">{prod.title}</div>
+                                <p className="text-gray-500">${prod.price}</p>
+                            </div>
+                        </div>
+                    )
+                })}
             </div>
         )
     }
