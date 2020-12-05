cc1ad972f99dc61c3b96fedcc3e610d56579985b
meuequalsD <nirmalkrishnav@gmail.com>
2020-12-05 13:01:40 +0530

    changes to style of product and images added

--- src/components/Product.js
+++ src/components/Product.js
@@ -6,13 +6,26 @@ export default class Product extends Component {
             <div className="grid grid-flow-col grid-cols-3 gap-4">
                 {this.props.products.map((prod, index) => {
                     return (
-                        <div key={index} className="p-6 bg-white rounded-xl shadow-md flex">
-                            <div className="flex-shrink-0">
-                                <img className="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo" />
+                        <div key={index} className="p-6 bg-white shadow-sm">
+                            <div className="flex justify-center">
+                                <a href={"#" + prod._id}>
+                                    <img className="" src={prod.image} alt={prod.desc} />
+                                </a>
                             </div>
                             <div>
-                                <div className="text-xl font-medium text-black">{prod.title}</div>
-                                <p className="text-gray-500">${prod.price}</p>
+                                <a href={"#" + prod.id}>
+                                    <div className="text-sm font-medium text-black">{prod.title}</div>
+                                </a>
+                            </div>
+                            <div className="flex content-center justify-between my-3">
+                                <div className="text-3xl text-gray-500">
+                                    ${prod.price}
+                                </div>
+                                <div>
+                                    <button class="flex-shrink-0 bg-purple-600 text-white text-base font-semibold py-2 px-4 rounded-lg shadow-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 focus:ring-offset-purple-200">
+                                        Add to cart
+                                </button>
+                                </div>
                             </div>
                         </div>
                     )
