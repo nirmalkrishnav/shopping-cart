cd4e2a976ce9b2c43dfea3fdf131fcb79d8c15dd
meuequalsD <nirmalkrishnav@gmail.com>
2020-12-08 21:33:16 +0530

    Adding express server, mongo atlas connection basic endpoints

---
+++ server.js
@@ -0,0 +1,49 @@
+require('dotenv').config()
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const shortId = require('shortid');
+
+const port = process.env.PORT || 27000;
+
+const mongoUrl = process.env.DB_URL;
+
+var app = express();
+app.use(bodyParser.json());
+
+mongoose.connect(mongoUrl, {
+    useNewUrlParser: true,
+    userCreateIndex: true,
+    useUnifiedTopology: true
+})
+
+const Product = mongoose.model('products', new mongoose.Schema(
+    {
+        _id: { type:String, default: shortId.generate },
+        title: String,
+        image: String,
+        desc: String,
+        sizes: [String],
+        price: Number,
+    }
+));
+
+
+app.get('/api/products', async (req, res) => {
+    const products = await Product.find({})
+    res.send(products);
+});
+
+app.post('/api/products/create', async (req, res) => {
+    const newProduct = new Product(req.body);
+    const result = await newProduct.save();
+    res.send(result);
+});
+
+app.delete('/api/product/:id', async (req, res) => {
+    const id = req.params.id;
+    const deletedProduct = await Product.findByIdAndDelete(id);
+    res.send(deletedProduct);
+});
+
+app.listen(port, () => console.log(`Server is active at 127.0.0.1:${port}`));
